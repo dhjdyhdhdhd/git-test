71c2e265536d9fd3079b1c2e1f7285467c364fdd
haiyu <haiyu@good.com>
2022-07-01 11:47:11 +0800

    hot-fix commit

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -1,6 +1,6 @@
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello Git！");
-        System.out.println("hello Git!2");
+        System.out.println("hello Git!3");
     }
 }
