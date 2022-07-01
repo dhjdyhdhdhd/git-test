8ae1ba33427d0a9128a2d5c0b3e8f607447cf234
haiyu <haiyu@good.com>
2022-07-01 15:10:15 +0800

    master commit2

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -2,5 +2,7 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello Git！");
         System.out.println("hello Git!3");
+        System.out.println("master test！");
+
     }
 }
